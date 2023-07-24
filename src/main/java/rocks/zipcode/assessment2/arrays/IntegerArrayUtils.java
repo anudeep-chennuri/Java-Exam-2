fc977856cec8ee3c3b7cc31962658739eeb991f8
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-24 10:56:43 -0400

    [completed] IntegerArrayUtils

--- src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
@@ -5,23 +5,54 @@ package rocks.zipcode.assessment2.arrays;
  */
 public class IntegerArrayUtils {
     /**
-     * @param integerArray - array to have value added to it
+     * @param integerArray   - array to have value added to it
      * @param valueToBeAdded - value to be added to the end of the array
      * @return - identical array with one additional element of `valueToBeAdded` at the end of the array
      */
     public static Integer[] add(Integer[] integerArray, Integer valueToBeAdded) {
-        return null;
+
+        Integer[] newArray = new Integer[integerArray.length + 1];
+
+        for (int i = 0; i < integerArray.length; i++) {
+            newArray[i] = integerArray[i];
+        }
+        newArray[newArray.length - 1] = valueToBeAdded;
+        return newArray;
     }
 
     /**
-     * @param integerArray - array to be manipulated
-     * @param indexToInsertAt - index of the element to be inserted at
+     * @param integerArray      - array to be manipulated
+     * @param indexToInsertAt   - index of the element to be inserted at
      * @param valueToBeInserted - value of the element to be inserted
      * @return `integerArray` with `valueToBeInserted` at index number `indexToInsertAt`
      */
-    public static Integer[] replace(Integer[] integerArray, int indexToInsertAt, Integer valueToBeInserted) {
-        return null;
+    public static Integer[] replace(Integer[] integerArray, Integer indexToInsertAt, Integer valueToBeInserted) {
+
+//        for (int i = 0; i < indexToInsertAt; i++) {
+//            newArray[i] = integerArray[i];
+//        }
+//
+//
+//        newArray[indexToInsertAt] = valueToBeInserted;
+//
+//        for (int i = indexToInsertAt + 1; i < newArray.length; i++){
+//            newArray[i] = integerArray[i-1];
+//        }
+
+        Integer[] newArray = new Integer[integerArray.length];
+
+        for (int i = 0; i < integerArray.length; i++) {
+            if (i == indexToInsertAt) {
+                newArray[i] = valueToBeInserted;
+            } else {
+                newArray[i] = integerArray[i];
+            }
+        }
+
+        return newArray;
+
     }
+
 
     /**
      * @param integerArray - array to be evaluated
@@ -29,7 +60,8 @@ public class IntegerArrayUtils {
      * @return element located at `indexToFetch`
      */
     public static Integer get(Integer[] integerArray, Integer indexToFetch) {
-        return null;
+
+        return integerArray[indexToFetch];
     }
 
     /**
@@ -37,7 +69,17 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1 and odd-values decremented by 1
      */
     public static Integer[] incrementEvenDecrementOdd(Integer[] integerArray) {
-        return null;
+
+        Integer[] newArray = new Integer[integerArray.length];
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 == 0) {
+                newArray[i] = integerArray[i] + 1;
+            } else if (integerArray[i] % 2 == 1){
+                newArray[i] = integerArray[i] - 1;
+            }
+        }
+        return newArray;
+
     }
 
     /**
@@ -45,14 +87,34 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1
      */
     public static Integer[] incrementEven(Integer[] integerArray) {
-        return null;
+
+        Integer[] newArray = new Integer[integerArray.length];
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 == 0) {
+                newArray[i] = integerArray[i] + 1;
+            } else {
+                newArray[i] = integerArray[i];
+            }
+        }
+        return newArray;
     }
 
-    /**
-     * @param input - array to be evaluated
-     * @return identical array with odd-values decremented by 1
-     */
-    public static Integer[] decrementOdd(Integer[] input) {
-        return null;
+
+
+        /**
+         * @param input - array to be evaluated
+         * @return identical array with odd-values decremented by 1
+         */
+        public static Integer[] decrementOdd (Integer[]input){
+            Integer[] newArray = new Integer[input.length];
+            for (int i = 0; i < input.length; i++) {
+                if (input[i] % 2 == 1) {
+                    newArray[i] = input[i] - 1;
+                } else {
+                    newArray[i] = input[i];
+                }
+            }
+            return newArray;
+        }
     }
-}
+
