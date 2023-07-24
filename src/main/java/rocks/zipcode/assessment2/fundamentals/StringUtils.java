aa187fb6a7b8952f9a24f3b0014ed2330e9d9f39
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-24 15:56:01 -0400

    [completed] String Utils

--- src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
@@ -6,29 +6,42 @@ package rocks.zipcode.assessment2.fundamentals;
 public class StringUtils {
     /**
      * @param stringToBePadded - string value to be flushed right
-     * @param amountOfPadding - amount of padding to be flushed left
+     * @param amountOfPadding  - amount of padding to be flushed left
      * @return `stringToBePadded` flushed right by left-padding
      */
     public static String padLeft(String stringToBePadded, int amountOfPadding) {
-        return null;
+        String newString = stringToBePadded.format("%1$" + amountOfPadding + "s", stringToBePadded);
+        return newString;
     }
 
     /**
      * @param stringToBePadded - string value to be flushed left
-     * @param amountOfPadding - amount of padding to be flushed right
+     * @param amountOfPadding  - amount of padding to be flushed right
      * @return `stringToBePadded` flushed right by right-padding
      */
     public static String padRight(String stringToBePadded, int amountOfPadding) {
-        return null;
+        String newString = stringToBePadded.format("%1$-" + amountOfPadding + "s", stringToBePadded);
+        return newString;
     }
 
     /**
-     * @param stringToBeRepeated - string value to be repeated
+     * @param stringToBeRepeated   - string value to be repeated
      * @param numberOfTimeToRepeat - number of times to repeat `stringToBeRepeated`
      * @return the string repeated and concatenated `n` times
      */
     public static String repeatString(String stringToBeRepeated, int numberOfTimeToRepeat) {
-        return null;
+
+//      smh
+//        stringToBeRepeated.repeat(numberOfTimeToRepeat);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numberOfTimeToRepeat; i++) {
+            sb.append(stringToBeRepeated);
+        }
+        String newString = sb.toString();
+        return newString;
+
+
     }
 
     /**
@@ -36,7 +49,13 @@ public class StringUtils {
      * @return - true if string only contains alpha characters
      */
     public static Boolean isAlphaString(String string) {
-        return null;
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLetter(string.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+
     }
 
     /**
@@ -44,14 +63,29 @@ public class StringUtils {
      * @return - true if string only contains numeric characters
      */
     public static Boolean isNumericString(String string) {
-        return null;
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isDigit(string.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+
     }
+
 
     /**
      * @param string - string to be evaluated
      * @return - true if string only contains special characters
      */
     public static Boolean isSpecialCharacterString(String string) {
-        return null;
-    }
-}
+
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLetterOrDigit(string.charAt(i)))
+                return false;
+        }
+
+    return true;
+}}
+
+
+
