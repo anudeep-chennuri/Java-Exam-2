4600cf98f3f45b214d52446692baa5bc1acacc1c
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-24 13:37:17 -0400

    [completed] StringArrayUtils

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 28/11/2018.
  */
@@ -12,11 +14,18 @@ public class StringArrayUtils {
      */
     public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
 
-        int len = endingIndex - startingIndex;
-       String[] newArray = new String[len];
+//        btw idk why this wouldnt work smh
+//        int len = endingIndex - startingIndex;
+//       String[] newArray = new String[len];
+//
+//
+//        System.arraycopy(arrayToBeSpliced, startingIndex, newArray, endingIndex - 1, len);
+//        return newArray;
 
-
-        System.arraycopy(arrayToBeSpliced, startingIndex, newArray, endingIndex - 1, len);
+        String [] newArray = new String[endingIndex - startingIndex];
+        for (int i = 0; i < (endingIndex - startingIndex); i++){
+            newArray[i] = arrayToBeSpliced[startingIndex + i];
+        }
         return newArray;
 
 
@@ -30,14 +39,13 @@ public class StringArrayUtils {
      * @return an array all elements between after `startingIndex`
      */
     public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
+        String [] newArray = new String[arrayToBeSpliced.length - startingIndex];
+        for (int i = 0; i < (arrayToBeSpliced.length - startingIndex); i++){
+            newArray[i] = arrayToBeSpliced[startingIndex + i];
+        }
+        return newArray;
 
-        int len = arrayToBeSpliced.length - startingIndex;
-        String[] newArray = new String[len];
 
-
-      System.arraycopy(arrayToBeSpliced, startingIndex, newArray, startingIndex, len);
-
-      return newArray;
 
 
 
